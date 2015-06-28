a36dae90d3ad3b5eb804c9fe70ac970956a891ac
Guillaume Bury <guillaume.bury@gmail.com>
2015-06-29 00:06:07 +0200

    Some more examples

--- test.ml
+++ test.ml
@@ -1,20 +1,22 @@
 
 (* Prelude *)
-module H = Hashtbl.Make(struct type t = int let hash i = i let equal = (=) end)
+module M = Map.Make(struct type t = int let compare = Pervasives.compare end)
 
-let h = H.create 2
-let () = H.add h 1 2
-let () = H.add h 2 3
-let () = H.add h 3 5
-let () = H.add h 17 42
+let m1 = M.(add 1 "a" (add 3 "c" (add 2 "b" empty)))
+let m2 = M.remove 2 m1
+
+let rec l = 1 :: 2 :: 3 :: l
 
 ;;
 (* Printing *)
 Dot.to_file "temp.gv" [
   "int_list", Repr.repr [1;2;3];
   "float_list", Repr.repr [1.;2.;3.];
-  "float_tuple", Repr.repr [| 4.; 5. |];
-  "hashtbl", Repr.repr h;
+  "circular_list", Repr.repr l;
+  "float_tuple", Repr.repr (9., 42.);
+  "float_array", Repr.repr [| 4.; 5.; 6.; 7.; 8.; 9. |];
+  "map1", Repr.repr m1;
+  "map2",Repr.repr m2;
 ]
 
 
