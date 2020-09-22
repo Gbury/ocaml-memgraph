017f41ef8439144c5071ece0de9c80266589efb2
Guillaume Bury <guillaume.bury@gmail.com>
2020-09-22 19:53:44 +0200

    Add example for Dolmen simplified AST

---
+++ examples/ast/conj/example.ml
@@ -0,0 +1,67 @@
+
+type location = {
+  file : string;
+  start_line : int;
+  start_column : int;
+  stop_line : int;
+  stop_column : int;
+}
+
+type namespace =
+  | Var
+  | Type
+  | Term
+
+type id = {
+  ns : namespace;
+  name : string;
+}
+
+type builtin =
+  | True | False
+  | And | Or | Not
+
+type binder =
+  | Forall | Exists
+
+type descr =
+  | Symbol of id
+  | Builtin of builtin
+  | Colon of t * t
+  | App of t * t list
+  | Binder of binder * t list * t
+  | Match of t * (t * t) list
+
+and t = {
+  term : descr;
+  attr : t list;
+  loc : location option;
+}
+
+let loc_file = "example.dummy"
+
+let mk_loc l c n = {
+  file = loc_file;
+  start_line = l;
+  start_column = c;
+  stop_line = l;
+  stop_column = c + n;
+}
+
+let mk ?loc term = { term; loc; attr = []; }
+
+let true_ = mk ~loc:(mk_loc 1 1 1) (Builtin True)
+let false_ = mk ~loc:(mk_loc 1 3 1) (Builtin False)
+let and_ = mk ~loc:(mk_loc 1 2 1) (Builtin And)
+let t = mk ~loc:(mk_loc 1 1 3) (App (and_, [true_; false_]))
+
+let () =
+  Memgraph.Dot.print_list Format.std_formatter (
+    (Memgraph.Repr.context (fun ctx -> [
+        "true_", ctx.mk true_;
+        "false_", ctx.mk false_;
+        "and_", ctx.mk and_;
+        "conjunction", ctx.mk t;
+      ]))
+    )
+
