d95317ea703377c8d1dbad80e031539393508de5
Guillaume Bury <guillaume.bury@gmail.com>
2021-12-24 20:49:02 +0100

    some more in closure example svg

--- examples/closure/doc.ml
+++ examples/closure/doc.ml
@@ -5,12 +5,13 @@ let test () =
   let rec f x y = n + n + y + g x
   and g y = f y (y + 1) in
 
-  f
+  f, g
 
 let () =
-  let f = test () in
+  let f, g = test () in
   Memgraph.Repr.(context (fun ctx ->
     Memgraph.Dot.print_list Format.std_formatter [
       "f", ctx.mk f;
+      "g", ctx.mk g;
     ]))
 
