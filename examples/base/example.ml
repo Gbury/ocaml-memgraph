4aa70f421a2aedc7b4de25b57ba1713a236c53e3
Guillaume Bury <guillaume.bury@gmail.com>
2024-06-28 13:38:47 +0200

    typo

--- examples/base/example.ml
+++ examples/base/example.ml
@@ -7,8 +7,8 @@ let char = 'a'
 let () =
   Memgraph.Repr.(context (fun ctx ->
       Memgraph.Dot.print_list Format.std_formatter [
-        {|unit|}, ctx.mk unit;
-        {|char|}, ctx.mk char;
+        {|()|}, ctx.mk unit;
+        {|'a'|}, ctx.mk char;
         {|true|}, ctx.mk bool;
         {|13|}, ctx.mk int;
       ]))
